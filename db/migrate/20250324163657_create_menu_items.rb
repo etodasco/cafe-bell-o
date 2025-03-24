df5cdd3a62d6708adf986665233be9fcfc2e2cfc
Eric Todasco <erictodasco@me.com>
2025-03-24 12:37:52 -0400

    created Model

---
+++ db/migrate/20250324163657_create_menu_items.rb
@@ -0,0 +1,12 @@
+class CreateMenuItems < ActiveRecord::Migration[8.0]
+  def change
+    create_table :menu_items do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
