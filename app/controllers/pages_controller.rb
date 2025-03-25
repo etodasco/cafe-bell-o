98a0bc62d0ee185bb202f68484a920a845dffb8b
Eric Todasco <erictodasco@me.com>
2025-03-25 12:22:56 -0400

    updated locals config application.rn and more

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,19 @@
 class PagesController < ApplicationController
-  def menu
-    # Find the menu item for the current day
+  before_action :set_locale  # Set locale before every action
+
+  def home
+    # Fetch today's special for the home page
     @menu_item_for_today = MenuItem.find_by(day: Time.current.strftime('%A'))
+  end
+
+  def menu
+    # Fetch all menu items for the menu page
+    @menu_items = MenuItem.all
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = :'fr-CA'  # Explicitly set the French Canadian locale for the app
   end
 end
