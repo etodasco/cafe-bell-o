2caf2874b8630b0d82b87bf5e294df04bd0a00eb
Eric Todasco <erictodasco@me.com>
2025-03-27 10:00:25 -0400

    updated view pages to french language

--- app/helpers/menu_items_helper.rb
+++ app/helpers/menu_items_helper.rb
@@ -1,2 +1,16 @@
 module MenuItemsHelper
+  # Map for English to French days of the week
+  DAY_MAP_FRENCH = {
+    "Monday" => "lundi",
+    "Tuesday" => "mardi",
+    "Wednesday" => "mercredi",
+    "Thursday" => "jeudi",
+    "Friday" => "vendredi",
+    "Saturday" => "samedi",
+    "Sunday" => "dimanche"
+  }
+
+  def translate_day_to_french(day_in_english)
+    DAY_MAP_FRENCH[day_in_english] || day_in_english
+  end
 end
--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,6 +1,33 @@
 class MenuItem < ApplicationRecord
+  # Validations
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
   validates :category, presence: true
+
+  # Callback to translate the day before saving
+  before_save :translate_day_to_english
+
+  # Define the DAY_MAP constant at the class level
+  DAY_MAP = {
+    "lundi" => "Monday",
+    "mardi" => "Tuesday",
+    "mercredi" => "Wednesday",
+    "jeudi" => "Thursday",
+    "vendredi" => "Friday",
+    "samedi" => "Saturday",
+    "dimanche" => "Sunday"
+  }
+
+  private
+
+  # Method to translate day input from French to English
+  def translate_day(day_in_french)
+    DAY_MAP[day_in_french.downcase] || day_in_french
+  end
+
+  # Callback method that triggers translation before save
+  def translate_day_to_english
+    self.day = translate_day(self.day) if self.day.present?
+  end
 end
