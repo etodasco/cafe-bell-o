837da83b0686e11b59f08129a79cf85a31331155
Eric Todasco <erictodasco@me.com>
2025-03-27 14:26:03 -0400

    updated a bunch of css

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Cafebello
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    # Add the images directory to the asset paths
+    config.assets.paths << Rails.root.join("app", "assets", "images")
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
