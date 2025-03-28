4a224ce945e5fd4466c5de6a512b50873d35263a
Eric Todasco <erictodasco@me.com>
2025-03-28 17:21:05 -0400

    deleting javascript

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,3 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.3-4/app/assets/javascripts/rails-ujs.esm.js"
-pin "navbar", to: "navbar.js"  # If you have a custom navbar script
-pin "reviewsCarousel", to: "javascript/reviewsCarousel.js"
