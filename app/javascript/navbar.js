9687eeade9ecfa4213248ac3475ba251d1f72bcd
Eric Todasco <erictodasco@me.com>
2025-03-28 17:24:21 -0400

    deleted navbar.js

--- app/javascript/navbar.js
+++
@@ -1,7 +0,0 @@
-// JavaScript for toggling the navbar menu
-const navbarToggler = document.getElementById('navbar-toggler');
-const navbarLinks = document.getElementById('navbar-links');
-
-navbarToggler.addEventListener('click', () => {
-  navbarLinks.classList.toggle('open');
-});
