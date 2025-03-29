018fed537dfb216d9785c9677ede704ba0a66e18
Eric Todasco <erictodasco@me.com>
2025-03-29 14:18:14 -0400

    updated errors with javacript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,10 +6,3 @@ Rails.start();  // Initialize Rails UJS
 
 // Import Turbo
 import "@hotwired/turbo-rails";  // Import Turbo (this is enough for Turbo functionality)
-
-// Log to confirm everything is working
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('DOM fully loaded and Bootstrap initialized');
-});
-
-console.log("Turbo is loaded!");
