6dd7bb72de61754d54719dd47204dfb6b8e9abdf
Eric Todasco <erictodasco@me.com>
2025-05-24 08:51:33 -0400

    updated notice translation in base_controller to French

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -6,7 +6,7 @@ class Admin::BaseController < ApplicationController
 
   def ensure_admin_role
     unless current_user.role == "admin"
-      redirect_to root_path, alert: "Only admins can access this page."
+      redirect_to root_path, alert: "Seuls les administrateurs peuvent accéder à cette page."
     end
   end
 end
